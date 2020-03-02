c597f2fb33a31c095cd7bc4d994ab8a649e83040
unknown <jianbinlin4573@gmail.com>
2020-03-01 21:30:06 -0500

    iiiiiii

--- Assignment/1/A1_answer.m
+++ Assignment/1/A1_answer.m
@@ -1,3 +1,19 @@
 
 
 refer to lecture 6 slide 47
+
+
+%refer to lecture 6 slide 47
+
+%to claim a numeric vector
+
+t = [1 , 2, 3 ,4, 5]
+
+value = [ 10, 20, 30, 40,50]
+
+plot(t,value)
+
+% in order to see the graph, you need to close the other graph
+
+linspace(1,100)%create 100 evenly distributed number from x1 and x2.
+
