930cfa38973320346fc028e4dc352eaa4cca5cde
Kevin Lin <32544867+KevinJianLin@users.noreply.github.com>
2020-04-06 17:32:16 -0400

    ii

--- Project/Report/Turbidity_meter/Turbidity_Code_2/Turbidity_Code_2.ino
+++ Project/Report/Turbidity_meter/Turbidity_Code_2/Turbidity_Code_2.ino
@@ -6,9 +6,16 @@ void setup() {
 void loop() {
   int sensorValue = analogRead(A1);
   float voltage = sensorValue * (5.0 / 1024.0);
-
+  float ntu = -1120.4*square(voltage)+5742.3*voltage-4353.8;
+  
   Serial.println ("Sensor Output (V):");
   Serial.println (voltage);
   Serial.println();
+
+  Serial.println (" Turbidity reading is (NTU):");
+  Serial.println (ntu);
+  Serial.println();
+
+  
   delay(1000);
 }
--- Project/Report/Turbidity_meter/Turbidity_meter.ino
+++ Project/Report/Turbidity_meter/Turbidity_meter.ino
@@ -6,7 +6,23 @@ Serial.begin(9600); //Baud rate: 9600
 void loop() {
   // put your main code here, to run repeatedly:
  int sensorValue = analogRead(A0);// read the input on analog pin 0:
+  
+  
+  
   float voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  
+  //Serial.println ("Sensor Output (V):");
+  //float ntu = -1120.4*square(voltage)+5742.3*voltage-4353.8;
+  
+  
   Serial.println(voltage); // print out the value you read:
+
+
+  Serial.println (" Turbidity reading is (NTU):");
+  Serial.println (ntu);
+  Serial.println();
+
+  
+  
   delay(500);
 }
